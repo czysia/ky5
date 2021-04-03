b90fe4cdd39e6ae702f0e5dadf84b3770ccf0865
czysia <nson02902@gmail.com>
2021-04-03 13:49:11 +0700

    commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var expressHDS= require('express-handlebars');
+
+var app = express()
+
+app.engine('handlebars',expressHDS({
+    layoutsDir: __dirname+'/views/layouts',
+    defaultLayout:'main'
+}))
+app.set('view engine','handlebars')
+
+app.get('/',function (request,response) {
+
+    response.render('home')
+});
+app.post('/',function (request,response) {
+
+});
+app.listen(process.env.PORT||'3000')
